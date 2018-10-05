fc752d113a02ac4d37b1aafafc8f280f7c7c79b2
xyq608074 <xyq608074@163.com>
2018-10-05 12:10:47 +0800

    JavaFx例子

---
+++ src/showTime/Controller.java
@@ -0,0 +1,36 @@
+package showTime;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Button btn;
+
+    @FXML
+    private TextField txt;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void showTime(ActionEvent event){
+        System.out.println("btn click");
+
+        Date date=new Date();
+        DateFormat df= new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+        String format = df.format(date);
+        txt.setText(format);
+    }
+}
